130c6b62e232fd4d3a2e08166914cf40613adccf
Kazuya Goto <kaz.goto.jp@gmail.com>
2025-08-17 14:40:33 +0900

    modify: test configus

---
+++ KAiWaMemoUnitTests/KAiWaMemoUnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  KAiWaMemoUnitTests.swift
+//  KAiWaMemoUnitTests
+//
+//  Created by Kazuya Goto on 2025/08/17.
+//
+
+import Testing
+
+struct KAiWaMemoUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
